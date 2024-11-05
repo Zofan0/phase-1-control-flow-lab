f13055bdfec411cb3257dc8bdc3fb26e1f54c2f8
Zofan0 <zpshark@gmail.com>
2024-11-05 23:01:02 +1000

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a){
+  if (a <= 400) {
+    return 'This one is on me!'
+  } 
+  if (a <= 2000 && a >= 400) {
+    return 'That will be twenty bucks.'
+  }
+  if (a >= 2500) {
+    return 'No can do.'
+  } else {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(a){
+  if (a == 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(a){
+  if (a == 'generous') {
+    return 'Thank you so much.'
+  }
+  if (a == 'not as generous') {
+    return 'Thank you.'
+  } else {
+    return 'Bye.'
+  }
 }
